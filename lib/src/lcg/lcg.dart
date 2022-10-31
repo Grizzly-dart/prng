92cbe6573766894e37014727e3c5da08779b1360
Ravi Teja Gudapati <tejainece@gmail.com>
2022-10-31 04:15:49 +0100

    LCG mapping

--- lib/src/lcg/lcg.dart
+++ lib/src/lcg/lcg.dart
@@ -16,3 +16,13 @@ class LCG {
     return _state;
   }
 }
+
+class LCGMapping {
+  final int a;
+  final int c;
+  final int m;
+
+  LCGMapping(this.a, this.c, this.m);
+
+  int transform(int i) => (i * a + c) % m;
+}
